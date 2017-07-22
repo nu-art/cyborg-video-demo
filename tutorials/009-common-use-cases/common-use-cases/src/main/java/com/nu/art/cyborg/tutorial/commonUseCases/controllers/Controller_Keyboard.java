6de84a4406c33aec06e02d64c8d35bc34f18f9bb
TacB0sS <Adam.Zehavi@nu-art-software.com>
2017-07-23 00:28:16 +0300

    keyboard example

--- tutorials/009-common-use-cases/common-use-cases/src/main/java/com/nu/art/cyborg/tutorial/commonUseCases/controllers/Controller_Keyboard.java
+++ tutorials/009-common-use-cases/common-use-cases/src/main/java/com/nu/art/cyborg/tutorial/commonUseCases/controllers/Controller_Keyboard.java
@@ -1,16 +1,30 @@
 package com.nu.art.cyborg.tutorial.commonUseCases.controllers;
 
+import com.nu.art.cyborg.common.interfaces.StringResourceResolver;
 import com.nu.art.cyborg.core.CyborgController;
+import com.nu.art.cyborg.core.KeyboardChangeListener.OnKeyboardVisibilityListener;
+import com.nu.art.cyborg.core.abs.Cyborg;
 import com.nu.art.cyborg.tutorial.commonUseCases.R;
 
 /**
  * Created by TacB0sS on 15-Jul 2017.
  */
 
-public  class Controller_Keyboard
-		extends CyborgController {
+public class Controller_Keyboard
+		extends CyborgController
+		implements OnKeyboardVisibilityListener {
 
 	public Controller_Keyboard() {
-		super(R.layout.controller__permissions);
+		super(R.layout.controller__keyboard);
+	}
+
+	@Override
+	public void onVisibilityChanged(final boolean visible) {
+		toastShort(new StringResourceResolver() {
+			@Override
+			public String getString(Cyborg cyborg) {
+				return "Keyboard " + (visible ? "VISIBLE" : "GONE");
+			}
+		});
 	}
 }
