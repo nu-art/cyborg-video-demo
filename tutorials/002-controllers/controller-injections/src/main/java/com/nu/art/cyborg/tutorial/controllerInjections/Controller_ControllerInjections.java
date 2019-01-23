25fd5e3219beda4aff9a099587ca86663f74a095
TacB0sS <adam.vdk@nu-art-software.com>
2019-01-23 08:35:48 +0200

    fix tutorial generation incomplete naming, started example for injections within controllers

---
+++ tutorials/002-controllers/controller-injections/src/main/java/com/nu/art/cyborg/tutorial/controllerInjections/Controller_ControllerInjections.java
@@ -0,0 +1,18 @@
+package com.nu.art.cyborg.tutorial.controllerInjections;
+
+import com.nu.art.cyborg.core.CyborgController;
+
+/**
+ * Created by TacB0sS on 12-May 2017.
+ */
+
+public class Controller_ControllerInjections
+	extends CyborgController {
+
+	private Module_ControllerInjections module;
+
+	public Controller_ControllerInjections() {
+		super(R.layout.controller__controller_injections);
+	}
+}
+
