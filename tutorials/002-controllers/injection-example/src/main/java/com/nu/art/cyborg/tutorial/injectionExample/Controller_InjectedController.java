ccb357a9f0fc2ad1e5dc9f136d30f55228cb4e8a
TacB0sS <adam.vdk@nu-art-software.com>
2019-01-23 23:56:37 +0200

    added injection example

---
+++ tutorials/002-controllers/injection-example/src/main/java/com/nu/art/cyborg/tutorial/injectionExample/Controller_InjectedController.java
@@ -0,0 +1,11 @@
+package com.nu.art.cyborg.tutorial.injectionExample;
+
+import com.nu.art.cyborg.core.CyborgController;
+
+public class Controller_InjectedController
+	extends CyborgController {
+
+	private Controller_InjectedController() {
+		super(R.layout.controller__injected_controller);
+	}
+}
