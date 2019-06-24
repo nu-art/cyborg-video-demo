c48efc5605767f8eabd54479b4e8238ec8511891
TacB0sS <adam.vdk@nu-art-software.com>
2019-06-24 10:39:57 +0300

    optimization for logcat to file logger

--- tutorials/010-media/media-player/src/main/java/com/nu/art/cyborg/tutorial/mediaPlayer/Controller_MediaPlayer.java
+++ tutorials/010-media/media-player/src/main/java/com/nu/art/cyborg/tutorial/mediaPlayer/Controller_MediaPlayer.java
@@ -1,26 +1,63 @@
 package com.nu.art.cyborg.tutorial.mediaPlayer;
 
+import android.view.View;
 import android.widget.TextView;
 
 import com.nu.art.cyborg.annotations.ViewIdentifier;
 import com.nu.art.cyborg.common.consts.ViewListener;
 import com.nu.art.cyborg.core.CyborgController;
+import com.nu.art.cyborg.media.CyborgMediaPlayer;
+import com.nu.art.cyborg.media.CyborgMediaPlayer.MediaPlayerListener;
+import com.nu.art.cyborg.media.CyborgMediaPlayer.MediaPlayerListenerImpl;
+import com.nu.art.cyborg.media.CyborgMediaPlayer.PlayerState;
 
 public class Controller_MediaPlayer
 	extends CyborgController {
 
 	@ViewIdentifier(
-		viewId = R.id.TV_State,
+		viewId = R.id.TV_PlayPause,
 		listeners = {
-			ViewListener.OnClick,
-			ViewListener.OnLongClick
+			ViewListener.OnClick
 		})
-	private TextView state;
+	private TextView playPause;
 
 	private Module_MediaPlayer module;
+	private CyborgMediaPlayer player;
 
 	public Controller_MediaPlayer() {
 		super(R.layout.controller__media_player);
+	}
+
+	@Override
+	protected void onCreate() {
+		this.player = module.getPlayer();
+	}
+
+	@Override
+	protected void onResume() {
+		renderUI();
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (player.isState(PlayerState.Idle))
+			player.createBuilder()
+			      .setUri("https://www.myinstants.com/media/sounds/goodbadugly-whistle-long.mp3")
+			      .setAutoPlay(true)
+			      .prepare();
+
+		if (player.isState(PlayerState.Preparing))
+			return;
+
+		if (player.isPlaying())
+			player.pause();
+		else
+			player.play();
+	}
+
+	@Override
+	protected void render() {
+		playPause.setText(player.isPlaying() ? "Pause" : "Play");
 	}
 }
 
--- tutorials/010-media/media-player/src/main/java/com/nu/art/cyborg/tutorial/mediaPlayer/Module_MediaPlayer.java
+++ tutorials/010-media/media-player/src/main/java/com/nu/art/cyborg/tutorial/mediaPlayer/Module_MediaPlayer.java
@@ -1,13 +1,21 @@
 package com.nu.art.cyborg.tutorial.mediaPlayer;
 
 import com.nu.art.cyborg.core.CyborgModule;
+import com.nu.art.cyborg.media.CyborgMediaPlayer;
 
 public class Module_MediaPlayer
 	extends CyborgModule {
+
+	private CyborgMediaPlayer player;
 
 	private Module_MediaPlayer() {}
 
 	@Override
 	protected void init() {
+		player = instantiateModuleItem(CyborgMediaPlayer.class);
+	}
+
+	public CyborgMediaPlayer getPlayer() {
+		return player;
 	}
 }
