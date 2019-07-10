2b6502635d43fca1715b6e689a5e62fa4760317f
TacB0sS <adam.vdk@nu-art-software.com>
2019-07-10 10:50:52 +0300

    belogged init twice

--- tutorials/011-services/service-keep-alive/src/main/java/com/nu/art/cyborg/tutorial/serviceKeepAlive/core/Module_ServiceKeepAlive.java
+++ tutorials/011-services/service-keep-alive/src/main/java/com/nu/art/cyborg/tutorial/serviceKeepAlive/core/Module_ServiceKeepAlive.java
@@ -1,17 +1,30 @@
 package com.nu.art.cyborg.tutorial.serviceKeepAlive.core;
 
+import android.app.PendingIntent;
+import android.os.Handler;
+
+import com.nu.art.core.tools.DateTimeTools;
 import com.nu.art.cyborg.common.interfaces.StringResourceResolver;
+import com.nu.art.cyborg.common.utils.BootStarterReceiver.OnBootCompletedListener;
+import com.nu.art.cyborg.core.CyborgBuilder;
 import com.nu.art.cyborg.core.CyborgModule;
 import com.nu.art.cyborg.core.abs.Cyborg;
+import com.nu.art.cyborg.core.modules.ThreadsModule;
 import com.nu.art.cyborg.modules.notifications.NotificationsModule;
+import com.nu.art.cyborg.modules.scheduler.Task;
+import com.nu.art.cyborg.modules.scheduler.TaskScheduler;
 
 public class Module_ServiceKeepAlive
-	extends CyborgModule {
+	extends CyborgModule
+	implements OnBootCompletedListener {
+	private Handler reviveHandler;
 
 	private Module_ServiceKeepAlive() {}
 
 	@Override
 	protected void init() {
+		reviveHandler = getModule(ThreadsModule.class).getDefaultHandler("Revive-Thread");
+		keepAlive();
 	}
 
 	final void handleActionB() {
@@ -35,4 +48,42 @@ public class Module_ServiceKeepAlive
 			}
 		});
 	}
+
+	public void keepAlive() {
+		reviveHandler.removeCallbacks(reviveRunnable);
+		reviveHandler.post(reviveRunnable);
+	}
+
+	@Override
+	public void onBootCompleted() {
+		keepAlive();
+	}
+
+	private Runnable reviveRunnable = new Runnable() {
+		PendingIntent lastIntent;
+
+		@Override
+		public void run() {
+			TaskScheduler taskScheduler = getModule(TaskScheduler.class);
+			if (lastIntent != null)
+				taskScheduler.cancel(lastIntent);
+
+			lastIntent = taskScheduler.scheduleTask(ReviveTask.class, null, ReviveTask.TaskId, System.currentTimeMillis() + 20 * DateTimeTools.Second);
+			reviveHandler.postDelayed(this, 15 * DateTimeTools.Second);
+		}
+	};
+
+	public static class ReviveTask
+		extends Task<String> {
+
+		public static final short TaskId = (short) "ReviveTask".hashCode();
+
+		protected ReviveTask() { super(String.class); }
+
+		@Override
+		protected void execute(String s) {
+			CyborgBuilder.getModule(Module_ServiceKeepAlive.class).keepAlive();
+		}
+	}
+
 }
