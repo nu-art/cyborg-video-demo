af4fc28095e659304248f2b43bcd8ba82700459a
Vangelis Gagkas <vangelis@intuitionrobotics.com>
2019-08-22 14:29:55 +0300

    Changes: - Save/clear a string value - Display this value on screen

--- tutorials/005-shared-preferences/shared-preferences/src/main/java/com/nu/art/cyborg/tutorial/sharedPreferences/Controller_Preferences.java
+++ tutorials/005-shared-preferences/shared-preferences/src/main/java/com/nu/art/cyborg/tutorial/sharedPreferences/Controller_Preferences.java
@@ -1,5 +1,12 @@
 package com.nu.art.cyborg.tutorial.sharedPreferences;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.nu.art.cyborg.annotations.ViewIdentifier;
+import com.nu.art.cyborg.common.consts.ViewListener;
 import com.nu.art.cyborg.core.CyborgController;
 
 /**
@@ -7,9 +14,56 @@ import com.nu.art.cyborg.core.CyborgController;
  */
 
 public class Controller_Preferences
-	extends CyborgController {
+	extends CyborgController
+	implements TestPrefsModule.TestPrefsModuleListener {
+
+	@ViewIdentifier(
+			viewId = R.id.btn_Clear,
+			listeners = {
+					ViewListener.OnClick,
+			})
+	private Button btnClearPrefs;
+
+	@ViewIdentifier(
+			viewId = R.id.btn_Save,
+			listeners = {
+					ViewListener.OnClick,
+			})
+	private Button btnSavePref;
+
+	@ViewIdentifier(
+			viewId = R.id.edit_InputValue)
+	private EditText editInputValues;
+
+	@ViewIdentifier(
+			viewId = R.id.txt_SavedValue)
+	private TextView txtSavedValue;
 
 	public Controller_Preferences() {
 		super(R.layout.controller__preferences);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		txtSavedValue.setText(getModule(TestPrefsModule.class).getString());
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.btn_Clear:
+				getModule(TestPrefsModule.class).clearStorage();
+				break;
+
+			case R.id.btn_Save:
+				getModule(TestPrefsModule.class).updateString(editInputValues.getText().toString());
+				break;
+		}
+	}
+
+	@Override
+	public void onStringValueChanged(String updatedValue) {
+		txtSavedValue.setText(updatedValue);
 	}
 }
